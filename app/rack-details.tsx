9ccab62ae514643e82e655453e90967dcfafecff
TriptoAfsin <AfsinTripto@gmail.com>
2023-04-01 12:22:42 +0600

    initial setup for tabs-ims

---
+++ app/rack-details.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box } from "native-base";
+
+export default function RackDetails() {
+  return (
+    <>
+      <Box>Rack Details</Box>
+    </>
+  );
+}
---
+++ app/scanner.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box } from "native-base";
+
+export default function Scanner() {
+  return (
+    <>
+      <Box>Scanner</Box>
+    </>
+  );
+}
